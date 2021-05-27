5b6710ffe9ec684c58426afef9d499b13bfb1756
preke <geek00021@gmail.com>
2021-05-27 22:19:13 +0800

    Update taskDes.py

--- asg/demo/taskDes.py
+++ asg/demo/taskDes.py
@@ -538,8 +538,8 @@ def introGen(fileID, df_selected, category_label, category_description):  # Intr
     sen_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
     whole_text = [i for i in df_selected.abstract] + [i for i in df_selected.intro]
     whole_text = sen_tokenizer.tokenize(' '.join(whole_text))
-    whole_text = [i.lower() for i in whole_text]
-    print(whole_text)
+    # whole_text = [i.lower() for i in whole_text]
+    # print(whole_text)
 
     topic_sents = []
     for t in topic:
